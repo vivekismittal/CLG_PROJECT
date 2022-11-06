7900a1d08f270360499001e1306968410a5c5d2c
traptigupta007 <traptigupta00712@gmail.com>
2022-11-06 22:43:55 +0530

    createClass

---
+++ BACKEND/subjectDetails.js
@@ -0,0 +1,22 @@
+subject = [
+    {
+        subjectName: "DBMS",
+        subjectCode: "KCS-503"
+    },
+    {
+        subjectName: "Compiler Design",
+        subjectCode: "KCS-502"
+    },
+    {
+        subjectName: "Artificial Intelligence",
+        subjectCode: "KCS-751"
+    },
+    {
+        subjectName: "Rural Development",
+        subjectCode: "KHU-078"
+    },
+    {
+        subjectName: "Computer Network",
+        subjectCode: "KCS-603"
+    }
+];
