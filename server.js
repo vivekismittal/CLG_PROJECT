d00c92dec91afa053030739af8323bbb604fbdf2
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-11-08 00:02:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,32 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const dbC = process.env.DATABASE_CLOUD; 
-const dbL = process.env.DATABASE_LOCAL; 
-const db = dbL;
-mongoose.connect(db).then(()=>console.log(`${db} \nDB connect succesfully!`));
+// const dbC = process.env.DATABASE_CLOUD; 
+// const dbL = process.env.DATABASE_LOCAL; 
+// const db = dbL;
+// mongoose.connect(db).then(()=>console.log(`${db} \nDB connect succesfully!`));
+
+
+
+const username = encodeURIComponent("nikhil9");
+const password = encodeURIComponent("Nikhil@123");
+
+let uri =
+	`mongodb+srv://${username}:${password}@cluster0.32rilsf.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(
+	uri,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		err && console.error(err);
+		console.log("Successfully connected to MongoDB: compilerdb");
+	}
+);
+
+
+
 
 const port = process.env.PORT || 8000;
 // const port = 51321;
